4d087bcbb5f1e03d35858eb4bc5a0288a8c1c34f
Micha <sprytnekonto69@gmail.com>
2025-01-18 21:48:17 +0100

    clean

--- client/client.py
+++ client/client.py
@@ -2,7 +2,7 @@ import asyncio
 import websockets
 import os
 async def test_client():
-    uri = "ws://192.168.100.10:8765"
+    uri = "ws://localhost:8765"
     async with websockets.connect(uri) as websocket:
         message = await websocket.recv()
         print(f"Server says: {message}")
